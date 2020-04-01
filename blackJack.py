656f5d5d899b277920b870f3b7c2387c997cb7fc
Ben Diegel <b.diegelg@gmail.com>
2020-04-01 00:30:31 -0400

    finished main functions

--- blackJack.py
+++ blackJack.py
@@ -5,10 +5,8 @@ import random
 from os import system
 
 #TODO: 
-    # - Add betting, split, insurance
-    # - Add play against dealer
+    # - Add split
     # - Add Xeria functionality
-    # - Basically finish programming the game b/c all i have rn is the framework lol 
 
 
 class Dealer:
@@ -21,6 +19,7 @@ class Dealer:
         self.deck = deck
         self.chips = chips
 
+    #builds the 52 card deck
     def buildDeck(self):
         self.deck.clear() #clears the deck in between uses to avoid over populating
         cardValues = [2,3,4,5,6,7,8,9,10,'Ace','Jack','Queen','King']
@@ -41,54 +40,32 @@ class Dealer:
     def removeCard(self, cardToRemove):
         self.deck.remove(cardToRemove)
 
+    #the dealer deals cards to itself until it hits a soft 17. 
+    def dealerTurn(self):
+        dealerAmt = getCardValue(self.mainHand, user = "dealer")
+        while dealerAmt < 17:
+            addToHand(self.mainHand, 1)
+            dealerAmt = getCardValue(self.mainHand, user = "dealer")
+        return dealerAmt
+
+
 
 class PlayerHand:
 
     def __init__(self,
                  mainHand = [],
-                 handValue = 0,
                  splitHand = [],
-                 chips = 100):
+                 chips = 100,
+                 betAmount = 0,
+                 insuranceBet = 0):
 
         self.mainHand = mainHand
-        self.handValue = handValue
         self.splitHand = splitHand
         self.chips = chips
+        self.betAmount = betAmount
+        self.insuranceBet = insuranceBet
 
-    #adds cards from dealer into their hand
-    def getCards(self, cards):
-        self.mainHand.extend(cards)
 
-    #totals all the cards in the mainHand
-    def getCardValue(self):
-        royals = ['Jack','Queen','King']
-        for x in self.mainHand:
-            for word in x.split():
-                if word.isdigit(): #checks for the number values then adds them
-                    self.handValue += int(word)
-                if word in royals: #checks for jacks queens and kings adds ten
-                    self.handValue += 10
-                if 'Ace' in word: #checks for aces and prompts the user to add 11 or 1
-                    while True:
-                        choice = input("\nAce can be an (11) or (1)?\n>>")
-                        if choice == '11': 
-                            self.handValue += 11 
-                            break
-                        elif choice == '1': 
-                            self.handValue += 1 
-                            break
-                        else: print("\nYou did not choose correctly")
-
-    #simple win condition to end the game
-    def winCondition(self):
-        system("clear")
-        self.getCardValue()
-        print("Your cards were ",str(self.mainHand))
-        print("With a total of: ",self.handValue)
-        if self.handValue <= 21:
-            print("You win!")
-        else:
-            print("You lose!")
 
 #creates the classes            
 Dealer = Dealer()
@@ -96,18 +73,110 @@ Player = PlayerHand()
 
 #main loop of the program
 def blackJackMain():
-    Dealer.buildDeck()
-    Player.getCards(Dealer.dealCards(2))
-
+    print("\n\tWelcome to Black Jack! Try and beat the dealer and make $$$.")
     while True:
         system("clear")
-        print("\nYou cards: ", str(Player.mainHand))
-        choice = input("\nType 'h' to get another card or 's' to stay\n>>")
-        if choice.lower() == 'h':
-            Player.getCards(Dealer.dealCards(1))
-        elif choice.lower() == 's':
-            Player.winCondition()
-            break
+        resetGame() #resets the hands and bet amounts
+        Player.betAmount = int(input("\nHow much would you like to bet? Total chips :"+str(Player.chips)+"\n>>"))
+
+        while True: #this is the main loop for the player
+            printHand(Player.mainHand)
+            choice = input("\nType 'h' to hit, 's' to stay, 'i' for insurance\n>>")
+            if choice.lower() == 'h': #adds a card to player hand
+                addToHand(Player.mainHand, 1)
+            elif choice.lower() == 'i': #pays 2:1 for betting the dealer will get 21
+                if Player.insuranceBet != 0:
+                    print("\nYou already have insurance!", str(Player.insuranceBet))
+                    input("Press enter\n")
+                else:
+                    Player.insuranceBet = int(input("\nHow much do you bet that Dealer gets 21?\n>>"))
+                    Player.chips -= Player.insuranceBet
+            elif choice.lower() == 's':
+                winCondition()
+                break
+
+#resets all the saved values
+def resetGame():
+    Dealer.buildDeck()
+    Player.mainHand.clear()
+    Dealer.mainHand.clear()
+    addToHand(Player.mainHand, 2)
+    addToHand(Dealer.mainHand, 2)
+    Player.insuranceBet = 0
+
+#adds any cards to players or dealers hand
+def addToHand(hand, amountOfCards):
+    hand.extend(Dealer.dealCards(amountOfCards))
+
+#totals all the cards in the mainHand
+def getCardValue(cards, user):
+    handValue = 0
+    royals = ['Jack','Queen','King']
+    for x in cards:
+        for word in x.split():
+            if word.isdigit(): #checks for the number values then adds them
+                handValue += int(word)
+            if word in royals: #checks for jacks queens and kings adds ten
+                handValue += 10
+            if 'Ace' in word and user == "player": #checks for aces and prompts the user to add 11 or 1
+                while True:
+                    printHand(cards)
+                    choice = input("\nAce can be an (11) or (1)?\n>>")
+                    if choice == '11': 
+                        handValue += 11 
+                        break
+                    elif choice == '1': 
+                        handValue += 1 
+                        break
+                    else: print("\nYou did not choose correctly")
+            elif 'Ace' in word and user == "dealer": #checks for dealers aces and makes them auto 11s
+                handValue += 11
+                break
+    return handValue
+
+#this prints the player and dealers hand with bet amount
+def printHand(hand):
+    system("clear")
+    print("\n\tDealer Cards: ")
+    for x in Dealer.mainHand: print("\t"+x)    
+    print("\n\nYour cards:\tYour bet: ", str(Player.betAmount))
+    for x in hand: print("\t"+x)
+
+#simple win condition to end the game
+def winCondition():
+    playerHandValue = getCardValue(Player.mainHand, user = "player") #these get the card values of the player and dealer
+    dealerHandValue = Dealer.dealerTurn()
+    printHand(Player.mainHand)
+    print("\nPlayer Total: ",playerHandValue,
+          "\tDealer Total:", dealerHandValue)
+    if dealerHandValue == 21: #if dealer gets 21 you lose unless you had an insurance bet
+        if Player.insuranceBet == 0:
+            lose()
+        else:
+            print("\nYou win! Here's ", str(Player.betAmount), " chips.")
+            print("\nAnd for betting insurance heres ", str(Player.insuranceBet*2), " chips.")
+            Player.chips += int(Player.betAmount)
+            Player.chips += int(Player.insuranceBet*2)
+    elif dealerHandValue > playerHandValue and dealerHandValue <= 21: #lose if dealer has higher card # 
+        lose() 
+    elif playerHandValue > 21: #lose if you bust
+        lose()
+    elif playerHandValue == 21: #3:1 payout for winning 21
+        print("\n21! You win 3:1 of your bet amount!\nHere's ", str(Player.betAmount*3), " chips.")
+        Player.chips += int(Player.betAmount*3)
+    else:
+        print("\nYou win! Here's ", str(Player.betAmount), " chips.")
+        Player.chips += int(Player.betAmount)
+    
+    choice = input("\nAgain? (y)(n)\n>>")
+    if choice.lower() == 'y':
+        return
+    else: exit()
+
+def lose():
+    print("\nYou lose! Thank you for ", str(Player.betAmount), " chips.")
+    Player.chips -= int(Player.betAmount)
+
 
 blackJackMain()
 
